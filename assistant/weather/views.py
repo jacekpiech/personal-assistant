f394ca1c9b75752afe9749484e9b1c8391bd5e30
TJacek <tjacekpiech@poczta.fm>
2024-05-13 18:03:13 +0200

    test pre-commit

--- assistant/weather/views.py
+++ assistant/weather/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Weather
+from .weather import save_weather_data
 
-# Create your views here.
+
+def weather_list(request):
+    save_weather_data()
+    weather_entries = Weather.objects.all()
+    return render(
+        request, "weather/post/weather_list.html", {"weather_entries": weather_entries}
+    )
+
+
+def weather_detail(request, pk):
+    weather_entry = get_object_or_404(Weather, pk=pk)
+    return render(
+        request, "weather/post/weather_detail.html", {"weather_entry": weather_entry}
+   )
---
+++ assistant/weather/weather.py
@@ -0,0 +1,23 @@
+from requests import get
+from .models import Weather
+from django.utils import timezone
+
+
+def save_weather_data():
+    response = get(f'https://danepubliczne.imgw.pl/api/data/synop/station/lodz/')
+    response_data: dict = response.json()
+    weather = Weather(
+        name=response_data['data_pomiaru'],
+        date=timezone.now(),
+        measurement_time=response_data['godzina_pomiaru'],
+        temperature=response_data['temperatura'],
+        humidity=response_data['wilgotnosc_wzgledna'],
+        pressure=response_data['cisnienie'],
+        wind_speed=response_data['predkosc_wiatru'],
+        rainfall=response_data['suma_opadu'],
+        air_quality=0,
+        data=response_data
+    )
+
+    if Weather.objects.count() == 0 or Weather.objects.latest('date').measurement_time != weather.measurement_time:
+        weather.save()
